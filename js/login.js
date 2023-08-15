75bf61de15b73044d059015756b4af35d217f064
hmsmiraz <hmsmiraz64729@gmail.com>
2023-08-16 01:51:31 +0600

    validation

--- js/login.js
+++ js/login.js
@@ -4,9 +4,9 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     const passwordField = document.getElementById('user-password');
     const password = passwordField.value;
     if(email === 'miraz@gmail.com' && password === '123'){
-        console.log('Valid User');
+        window.location.href = 'bank.html';
     }
     else{
-        console.log('Invalid User');
+        alert('Invalid User');
     }
 })
