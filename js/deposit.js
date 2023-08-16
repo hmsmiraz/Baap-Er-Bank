73389b99c0aa87975d9892b5c4eeb8c933e1cefd
hmsmiraz <hmsmiraz64729@gmail.com>
2023-08-16 10:11:33 +0600

    Update deposit and balance

--- js/deposit.js
+++ js/deposit.js
@@ -1,7 +1,23 @@
 document.getElementById('btn-deposit').addEventListener('click', function(){
+
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal =  parseFloat(previousDepositTotalString);
+
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = currentDepositTotal;
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const balanceTotalElementString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalElementString);
+
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+    depositField.value = '';
+
 })
